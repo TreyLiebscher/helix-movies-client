0993f306ce92b84816f0ab7ee28d6b2c06064c22
Trey Liebscher <wcliebsc@gmail.com>
2018-09-26 17:02:40 -0400

    cleanup

--- src/components/movieMatch.js
+++ src/components/movieMatch.js
@@ -61,7 +61,7 @@ export default class MovieMatch extends React.Component {
                         </ul>
                     </li>
                 ))
-            })
+            }, () => console.log(this.state))
             
         })
         
