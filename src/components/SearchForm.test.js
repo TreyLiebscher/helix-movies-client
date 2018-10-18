ed764f38c242709f55b4841dc67eb3bcd646f2a1
Trey Liebscher <wcliebsc@gmail.com>
2018-10-18 08:07:08 -0400

    modified test

--- src/components/SearchForm.test.js
+++ src/components/SearchForm.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {shallow, mount} from 'enzyme';
 
-import SearchFormR from './SearchForm';
+import SearchFormWRouter from './SearchForm';
 import {SearchForm} from './SearchForm';
 
 describe('<SearchForm />', () => {
@@ -9,12 +9,12 @@ describe('<SearchForm />', () => {
         shallow(<SearchForm />);
     });
 
-    // it('Should fire onSubmit callback when form is submitted', () => {
-    //     const callback = jest.fn();
-    //     const wrapper = mount(<SearchForm onSubmit={callback} />);
-    //     const value = 'Movie';
-    //     wrapper.find('input[type="text"]').instance().value = value;
-    //     wrapper.simulate('submit');
-    //     expect(callback).toHaveBeenCalledWith(value);
-    // });
+    it('Should fire onSubmit callback when form is submitted', () => {
+        const callback = jest.fn();
+        const wrapper = mount(<SearchForm onSubmit={callback} />);
+        const value = 'Movie';
+        wrapper.find('input[type="text"]').instance().value = value;
+        wrapper.simulate('submit');
+        expect(callback).toHaveBeenCalledWith(value);
+    });
 })
